283f36c4cdeda974475ac5fcf611472a0f6d36e0
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2020-12-04 00:26:21 -0600

    Se instalaron compontes para la base de datos

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import * as SplashScreen from "expo-splash-screen";
+import useDatabase from "./src/hooks/useDatabase";
+import { UsersContextProvier } from "./src/context/UsersContext";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -9,6 +13,7 @@ import DaydreamLogin from "./src/screens/DaydreamLogin";
 import DaydreamSignIn from "./src/screens/DaydreamSignIn";
 import DaydreamInfoPersonal from "./src/screens/DaydreamInfoPersonal";
 import DaydreamAvatarsProfile from "./src/screens/DaydreamAvatarsProfile";
+import DaydreamControlUser from "./src/screens/DaydreamControlUser";
 
 import { Dimensions } from 'react-native';
 
@@ -19,6 +24,12 @@ const { width, height } = Dimensions.get("window");
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  SplashScreen.preventAutoHideAsync();
+
+  const isLoadingComplete = useDatabase();
+
+  if (isLoadingComplete) SplashScreen.hideAsync();
   
   return (
       <NavigationContainer>
@@ -31,6 +42,8 @@ export default function App() {
           <Stack.Screen name = "SignIn" component = {DaydreamSignIn} options = {{ title: ""}}/>
           <Stack.Screen name = "InfoPersonal" component = {DaydreamInfoPersonal} options = {{ title: ""}}/>
           <Stack.Screen name = "AvartarsProfile" component = {DaydreamAvatarsProfile} options = {{ title: ""}}/>
+          <Stack.Screen name = "ControlUser" component = {DaydreamControlUser} options = {{ title: ""}}/>
+
         </Stack.Navigator>
       </NavigationContainer>
   );
