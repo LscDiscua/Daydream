1f52376c595f718ce93204d88699f27c33769143
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2020-12-15 08:37:05 -0600

    Se grearon los diferentes await para cada tabla dentro de hooks

--- src/hooks/useDatabase.js
+++ src/hooks/useDatabase.js
@@ -17,10 +17,28 @@ const useDatabase = () => {
             await database.setupDatabaseTableAsync();
             await database.setupUserAsync();
 
-            //----- Comidad------
+            //----- Comidas Componetes------
             // await database.dropDatabaseFoodTableAsync();
-            // await database.setupDatabaseFoodTableAsync();
-            // await database.setupFoodAsync();
+            await database.setupDatabaseFoodTableAsync();
+            await database.setupFoodAsync();
+
+
+            //---- Ejericio Componentes -----
+            // await database.dropDatabaseExerciseTableAsync();
+            await database.setupDatabaseExerciseTableAsync();
+            await database.setupExerciseAsync();
+
+
+            //--- Vasos de Agua componentes---
+            // await database.dropDatabaseCupTableAsync();
+            await database.setupDatabaseCupTableAsync();
+            await database.setupCupAsync();
+
+
+            //-- Plan
+            // await database.dropDatabasePlanTableAsync();
+            // await database.setupDatabasePlanTableAsync();
+            // await database.setupPlanAsync();
             
             // finaliza la carga de la DB
             setIsLoadingComplete(true);
